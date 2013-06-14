e1c557ad52a4dabf1a29e8eca6e46e33b0091d5b
vitor <vitor4neto@hotmail.com>
2013-06-14 12:58:52 +0100

    Adicionei ficheiro clientes.py

---
+++ projeto/util.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+
+def ler_ficheiro(ficheiro):
+	lista = []
+	try:
+		f = open(ficheiro, "rb")
+		lista = pickle.load(f)		
+		f.close
+	except:
+		print "Ficheiro %s não existe!" % (ficheiro)
+
+	return lista
+
+
+def escrever_ficheiro(ficheiro, lista):
+	try:
+		f = open(ficheiro, "wb")
+		pickle.dump(lista, f)		
+		f.close
+		print "Escrevi ficheiro %s" % (ficheiro)
+	except:
+		print "Erro a gravar ficheiro %s!" % (ficheiro)
+
+
+if __name__ == "__main__":
+    print "Este programa não deve ser executado diretamente"
